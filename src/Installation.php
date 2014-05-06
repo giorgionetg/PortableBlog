f498928c8e9af4e92d15a839216e2fda241aba8d
giorgionetg <giorgionetg@gmail.com>
2014-05-06 23:21:16 +0200

    Installation File

---
+++ src/Installation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PortableBlog;
+
+use Exception;
+
+class Installation {
+    
+    public function __construct($dbconn = array(), $tables = array())
+    {
+        if (empty($tables)) {
+            throw new Exception('There are any tables on setup');
+        }
+    }
+    
+    public function fails()
+    {
+        return False;
+    }
+}
